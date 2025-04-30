fb29326fd049cac717c59cab6a4ef1f89717c354
Zcy <zcy_chao1024@163.com>
2025-04-30 23:12:00 +0800

    init left navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+
+import {Navigation} from "@/components";
+
 export default function Home() {
     return (
-        <div>
-            test
+        <div className="w-screen h-screen bg-[#f1f2f5] text-[#0a0e0f]">
+            <div className="flex flex-col items-center">
+                <Navigation />
+                <div className="flex-1">test</div>
+            </div>
         </div>
     );
 }
