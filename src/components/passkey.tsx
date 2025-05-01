cc049342adad3f1e1f012778e873705c100429b0
Zcy <zcy_chao1024@163.com>
2025-05-01 12:37:35 +0800

    get sui balance && faucet link

--- src/components/passkey.tsx
+++ src/components/passkey.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import {Button} from "@/components/ui/button";
+import {HoverCard, HoverCardContent, HoverCardTrigger,} from "@/components/ui/hover-card"
 import {findCommonPublicKey, PasskeyKeypair} from "@mysten/sui/keypairs/passkey";
 import {getPasskeyProvider} from "@/configs/networkConfig";
 import {useAppSelector, AppDispatch} from "@/store";
 import {useDispatch} from "react-redux";
-import {setAddress} from "@/store/modules/info";
+import {refreshBalance, setAddress} from "@/store/modules/info";
 import {Copy} from "lucide-react";
 
 export default function PassKey() {
     const address = useAppSelector(state => state.info.address);
+    const balance = useAppSelector(state => state.info.balance);
     const dispatch = useDispatch<AppDispatch>();
 
     const handleCreateWallet = async () => {
@@ -44,16 +46,33 @@ export default function PassKey() {
         localStorage.setItem("PublicKey", passkey.getPublicKey().toRawBytes().toString());
     }
 
+    const openCard = (isOpen: boolean) => {
+        if (isOpen)
+            dispatch(refreshBalance(address));
+    }
+
     return (
         <div className="flex gap-10 items-center">
-            <div className="flex gap-3 items-center">
-                <span>{address ? address.slice(0, 6) + "..." + address.slice(-4) : ""}</span>
-                {
-                    address &&
-                    <Copy size={16} className="cursor-pointer active:text-[#196ae3]" onClick={() => navigator.clipboard.writeText(address)} />
-                }
-            </div>
-            <Button variant="ghost" className="cursor-pointer" onClick={handleCreateWallet}>Create Passkey Wallet</Button>
+            <HoverCard openDelay={100} onOpenChange={openCard}>
+                <HoverCardTrigger>
+                    <div className="flex gap-3 items-center">
+                        <span>{address ? address.slice(0, 6) + "..." + address.slice(-4) : ""}</span>
+                        {
+                            address &&
+                            <Copy size={16} className="cursor-pointer active:text-[#196ae3]"
+                                  onClick={() => navigator.clipboard.writeText(address)}/>
+                        }
+                    </div>
+                </HoverCardTrigger>
+                <HoverCardContent className="flex flex-col gap-3 items-center w-36 bg-[#222] text-[#afb3b5] border-2 border-[#0a0e0f]">
+                    <Button className="w-full bg-[#222]" disabled={true}>{`${balance} Sui`}</Button>
+                    <Button className="w-full bg-[#222]">
+                        <a href={`https://faucet.sui.io/?address=${address}`} target="_blank" rel="noopener noreferrer">Faucet</a>
+                    </Button>
+                </HoverCardContent>
+            </HoverCard>
+            <Button variant="ghost" className="cursor-pointer" onClick={handleCreateWallet}>Create Passkey
+                Wallet</Button>
             <Button variant="ghost" className="cursor-pointer" onClick={handleLoadWallet}>Load Passkey Wallet</Button>
         </div>
     );
--- src/store/modules/info.ts
+++ src/store/modules/info.ts
@@ -4,13 +4,16 @@ import {createSlice, ThunkDispatch, UnknownAction} from "@reduxjs/toolkit";
 import {Dispatch} from "react";
 import {PasskeyKeypair} from "@mysten/sui/keypairs/passkey";
 import {getPasskeyProvider} from "@/configs/networkConfig";
+import {getBalance} from "@/lib/contracts";
 
 export type initialStateType = {
     address: string,
+    balance: string,
 }
 
 const initialState: initialStateType = {
     address: "",
+    balance: "0",
 }
 
 const infoStore = createSlice({
@@ -20,6 +23,9 @@ const infoStore = createSlice({
         setAddress(state, action: {payload: string}) {
             state.address = action.payload;
         },
+        setBalance(state, action: {payload: string}) {
+            state.balance = action.payload;
+        }
     }
 });
 
@@ -31,18 +37,35 @@ const refreshAll = (publicKeyStr: string | null | undefined) => {
             const publicKey = new Uint8Array(publicKeyStr.split(',').map(item => Number(item)));
             const keypair = new PasskeyKeypair(publicKey, getPasskeyProvider(window.location.hostname));
             dispatch(setAddress(keypair.toSuiAddress()));
+            dispatch(setBalance(await getBalance(keypair.toSuiAddress())));
+            return;
         }
+        dispatch(setAddress(""));
+        dispatch(setBalance("0"));
+    }
+}
+
+const refreshBalance = (owner: string) => {
+    return async (dispatch: ThunkDispatch<{
+        info: initialStateType
+    }, undefined, UnknownAction> & Dispatch<UnknownAction>) => {
+        dispatch(setBalance(await getBalance(owner)));
     }
 }
 
 const {
     setAddress,
+    setBalance,
 } = infoStore.actions;
 
 export {
     setAddress,
+    setBalance,
 };
 
-export {refreshAll};
+export {
+    refreshAll,
+    refreshBalance,
+};
 
 export default infoStore.reducer;
